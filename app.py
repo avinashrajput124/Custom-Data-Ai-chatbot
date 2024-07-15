eb1615701f483c7620ee605444f074d25c78f4f7
Avinash Singh <rajputavinash124@gmail.com>
2024-07-15 17:37:26 +0530

    adeed app .py

--- app.py
+++ app.py
@@ -110,6 +110,7 @@ keywords_for_details = [
     "thorough explanation",
     "deep dive"
 ]
+
 def match_intent(user_input):
     for intent, keywords in basic_intents.items():
         for keyword in keywords:
@@ -134,4 +135,5 @@ def chat():
 
 if __name__ == '__main__':
     load_models()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
