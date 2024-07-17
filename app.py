2cdb4de80809fccca6480db597083388885701f4
Avinash Singh <rajputavinash124@gmail.com>
2024-07-17 14:09:50 +0530

    update main .py

--- app.py
+++ app.py
@@ -145,5 +145,5 @@ async def chat():
 
 if __name__ == '__main__':
     load_models()
-    app.run(debug=True, host='0.0.0.0')    
-    # app.run(debug=True)
+    # app.run(debug=True, host='0.0.0.0')    
+    app.run(debug=True)
